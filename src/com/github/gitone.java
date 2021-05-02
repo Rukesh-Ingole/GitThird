6bafc943c95184032e1ce814eaefc233f1a055db
RUKESH INGOLE <rukeshingole>
2021-05-02 13:21:38 +0530

    First Commit

---
+++ src/com/github/gitone.java
@@ -0,0 +1,17 @@
+package com.github;
+
+public class gitone 
+{
+	public static void main(String[] args) {
+String hungry = "Yes";
+
+if(hungry=="Yes" || hungry=="Y")
+{
+	System.out.println("This is hungry");
+}
+else
+{
+	System.out.println("This is not hungry");
+}
+}
+}
